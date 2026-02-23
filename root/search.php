5699020608da131e10c98a45a1a812b77f09931c
Tilo von Eschwege <tilo.voneschwege@gmail.com>
2026-02-23 17:42:41 +0100

    Renamed cdate to timestmp in search.php

--- root/search.php
+++ root/search.php
@@ -30,23 +30,17 @@
     $keywordfromform = $_GET["keyword"]; //Das keyword wird comment.php aus dem Eingabefeld zur Suchbegriffeingabe entnommen und einer Variable zugewiesen.
                                         //$keywordfromform wird unten zum Durchsuchen der Datenbanksuchanfrage-Ergebnisse genutzt
 
-    // Sucht in der Datenbank nach dem Wort chicken
-        echo "<h2>Show all comments with the word: $keywordfromform </h2>";
+    echo "<h2>Show all comments with the word: $keywordfromform </h2>";
 
-    $sql = "SELECT id, content, author, cdate FROM comments WHERE content LIKE '%$keywordfromform%' "; //Es werden alle Attribute von Kommentaren ausgewählt und nur die
-    $result = $conn->query($sql);                                                                                   // Kommentare ausgegeben die irgendwo in ihrem C_content das 
-                                                                                                                    // gesuchte Keyword beinhalten.
-
-
-    // Falls es passende Daten in der Datenbank gibt gebe diese wie folgt nacheinander aus
-    //Die while Schleife läuft solange bis result keine Treffer mehr hat, anders gesagt es gibt jede Spalte an Kommentaren aus bis es keine Spalte mehr gibt.
-    //Falls Es vom Start aus keine Ergebnisse gibt, gibt das Programm den Schriftzug "0 results" aus.
+    $sql = "SELECT * FROM comments WHERE content LIKE '%$keywordfromform%' "; 
+    $result = $conn->query($sql);                                                                                    
+                                                                                                                    
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-        echo "id " .$row["id"]. ": \"" . $row["content"]. "\", by: " . $row["author"]. " ".$row["cdate"]."<br>"; //CommentID, C_content, c_author, c_date, werden
-        }                                                                                                                                //alle in einer Zeile von jeweils einem Kommentar
+        echo "id " .$row["id"]. ": \"" . $row["content"]. "\", by: " . $row["author"]. " ".$row["timestmp"]."<br>"; 
+        }                                                                                                                    
     } else { 
-        echo "No comments with that keyword found!<br>";                                                                                                                        //ausgegeben
+        echo "No comments with that keyword found!<br>";                                                                                                                        
         echo "0 results";
                       
     }
