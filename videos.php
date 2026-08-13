b3799ea6dce53be3a13d79b98bfd58f7fab7eaa9
Tilo von Eschwege <tilo.voneschwege@gmail.com>
2026-08-13 17:05:37 +0200

    Removed docs

--- videos.php
+++ videos.php
@@ -19,9 +19,6 @@ include("header.php");
 <!----------------------------------------------------------
 -					    Experiments
 ----------------------------------------------------------->
-<a href="src/docs/Antrag_Einstellung_WS26_Tilo.docx" download>
-<a href="src/docs/Sozialversicherung_WS26_Tilo.docx" download>
-<a href="src/docs/Sozialversicherung_WS26_Tilo.odt" download>
 
 <div class="main">
 
